13ab831ceef026ed0d4a373a7ff497416b915014
kenrique-ngwa <ngwakenri2016@gmail.com>
2025-12-23 10:25:32 -0500

    fix about page

--- components/About.tsx
+++ components/About.tsx
@@ -12,7 +12,7 @@ export default function About({}: Props) {
       transition={{ duration: 1.5}}
 
     className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+      <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>
         About
         </h3>
 
@@ -26,7 +26,7 @@ export default function About({}: Props) {
           }}
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
-        src='https://avatars.githubusercontent.com/u/72971484?v=4'
+        src="/images/kenrique.jpg"
         className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[200px] xl:h-[300px]'
         />
         
